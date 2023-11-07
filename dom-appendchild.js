0406ca5347d583ae7b1deb85eeb2c15c07bee827
“enamul58” <enamullimon@gmail.com>
2023-11-07 23:35:26 +0600

    remove list item inside unordered list

--- dom-appendchild.js
+++ dom-appendchild.js
@@ -15,15 +15,20 @@ function addListItemOptimizeWay(language) {
   document.querySelector(".language").appendChild(li);
 }
 
-addListItemOptimizeWay("C");
+addListItemOptimizeWay("Golang");
 
 //For Edit list item inside unordered list
 // approch-01
 const secondLang = document.querySelector("li:nth-child(2)");
 const li = document.createElement("li");
-li.innerHTML = "golang";
+//li.innerHTML = "golang";
+li.textContent = "C";
 secondLang.replaceWith(li);
 
 //approch-02
 const firstLang = document.querySelector("li:first-child");
 firstLang.outerHTML = "<li>TypeScript</li>";
+
+//For remove list item inside unordered list
+const lastLang = document.querySelector("li:last-child");
+lastLang.remove();
